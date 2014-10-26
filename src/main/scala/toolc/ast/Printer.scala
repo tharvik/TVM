2fd09e678374c7b7c1ba60e844a97a93f3c8eb74
valerian-rousset <valerian.rousset@epfl.ch>
2014-10-26 17:39:56 +0100

    Add some parenthisis to Printer

--- src/main/scala/toolc/ast/Printer.scala
+++ src/main/scala/toolc/ast/Printer.scala
@@ -23,7 +23,7 @@ object Printer {
     Printer(0, t)
 
   def expr(repr: String, lhs: ExprTree, rhs: ExprTree): String =
-    Printer(lhs) + " " + repr + " " + Printer(rhs)
+    "(" + Printer(lhs) + ") " + repr + " (" + Printer(rhs) + ")"
 
 
   def apply(l: Int, t: Tree): String = {
@@ -44,20 +44,30 @@ object Printer {
           if(!parent.isEmpty) " extends " + Printer(parent.get) else ""
 
         "class " + Printer(id) + ext + " {" + eol +
+        eol +
         unroll(l + 1, vars, eol) + eol +
+        eol +
         unroll(l + 1, methods, eol) + eol +
         just(l) + "}"
       }
 
       case VarDecl(tpe: TypeTree, id: Identifier) =>
         "var " + Printer(id) + " : " + Printer(tpe) + ";"
-      case MethodDecl(retType: TypeTree, id: Identifier, args: List[Formal], vars: List[VarDecl], stats: List[StatTree], retExpr: ExprTree) =>
+      case MethodDecl(retType: TypeTree, id: Identifier, args: List[Formal], vars: List[VarDecl], stats: List[StatTree], retExpr: ExprTree) => {
+        val strVar =
+          if(vars.isEmpty) ""
+          else eol + unroll(l + 1, vars, eol) + eol
+        val strStats =
+          if(stats.isEmpty) ""
+          else eol + unroll(l + 1, stats, eol) + eol
+
         "def " + Printer(id) + "(" + unroll(0, args, ", ") + ") : " +
         Printer(retType) + " = {" + eol +
-        unroll(l + 1, vars, eol) + eol +
-        unroll(l + 1, stats, eol) + eol +
+        strVar +
+        strStats + eol +
         just(l + 1) + "return " + Printer(retExpr) + ";" + eol +
         just(l) + "}" + eol
+      }
       case Formal(tpe: TypeTree, id: Identifier) =>
         Printer(id) + " : " + Printer(tpe)
 
@@ -72,13 +82,13 @@ object Printer {
         just(l) + "}"
 
       case If(expr: ExprTree, thn: StatTree, els: Option[StatTree]) => {
-        val next = if(els.isEmpty) "" else "else " + Printer(els.get)
-        "if (" + Printer(expr) + ")" + Printer(thn) + next
+        val next = if(els.isEmpty) "" else "else " + Printer(l,els.get)
+        "if (" + Printer(expr) + ") " + Printer(l,thn) + next
       }
       case While(expr: ExprTree, stat: StatTree) =>
-        "while (" + Printer(expr) + ")" + Printer(stat)
+        "while (" + Printer(expr) + ") " + Printer(l, stat)
       case Println(expr: ExprTree) =>
-        just(l) + "println(" + Printer(expr) + ");"
+        "println(" + Printer(expr) + ");"
       case Assign(id: Identifier, expr: ExprTree) =>
         Printer(id) + " = " + Printer(expr) + ";"
       case ArrayAssign(id: Identifier, index: ExprTree, expr: ExprTree) =>
@@ -91,13 +101,13 @@ object Printer {
       case Times(lhs: ExprTree, rhs: ExprTree) => expr("*", lhs, rhs)
       case Div(lhs: ExprTree, rhs: ExprTree) => expr("/", lhs, rhs)
       case LessThan(lhs: ExprTree, rhs: ExprTree) => expr("<", lhs, rhs)
-      case Equals(lhs: ExprTree, rhs: ExprTree) => expr("=", lhs, rhs)
+      case Equals(lhs: ExprTree, rhs: ExprTree) => expr("==", lhs, rhs)
       case ArrayRead(arr: ExprTree, index: ExprTree) =>
-        Printer(arr) + "[" + Printer(index) + "]"
+        "(" + Printer(arr) + ")" + "[" + Printer(index) + "]"
       case ArrayLength(arr: ExprTree) =>
-        Printer(arr) + ".length"
+        "(" + Printer(arr) + ")" + ".length"
       case MethodCall(obj: ExprTree, meth: Identifier, args: List[ExprTree]) =>
-        Printer(obj) + "." + Printer(meth) + "(" + unroll(0, args, ", ") + ")"
+        "(" + Printer(obj) + ")." + Printer(meth) + "(" + unroll(0, args, ", ") + ")"
       case IntLit(value: Int) => value.toString
       case StringLit(value: String) => "\"" + value + "\""
 
@@ -107,7 +117,7 @@ object Printer {
       case This() => "this"
       case NewIntArray(size: ExprTree) => "new Int[" + Printer(size) + "]"
       case New(tpe: Identifier) => "new " + Printer(tpe) + "()"
-      case Not(expr: ExprTree) => "!" + Printer(expr)
+      case Not(expr: ExprTree) => "! (" + Printer(expr) + ")"
     }
   }
 }
