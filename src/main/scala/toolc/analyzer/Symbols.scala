1b1f9341f58a5d3de7dc9b91eba471fd7ff9bbc0
Etienne Kneuss <etienne.kneuss@epfl.ch>
2014-11-17 14:53:50 +0100

    Stubs for Lab05 - Type Checking

--- src/main/scala/toolc/analyzer/Symbols.scala
+++ src/main/scala/toolc/analyzer/Symbols.scala
@@ -2,6 +2,7 @@ package toolc
 package analyzer
 
 import utils._
+import Types._
 
 object Symbols {
   trait Symbolic[S <: Symbol] {
@@ -18,7 +19,7 @@ object Symbols {
     }
   }
 
-  sealed abstract class Symbol extends Positioned {
+  sealed abstract class Symbol extends Positioned with Typed {
     val id: Int = ID.next
     val name: String
   }
