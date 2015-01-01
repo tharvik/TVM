3100426cef898e148f4acc8d289aff5bc5d911e1
valerian-rousset <valerian.rousset@epfl.ch>
2015-01-01 16:49:58 +0100

    Add test for tvm

--- test.sh
+++ test.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-t='^Loading /.*/sbt-.*sbt-launch-lib.bash$'
-t="$t|^.\[0m\[.[\d+m(info|trace|error|success).\[0m\]"
-readonly sbt_cleanup="$t"
 readonly ref='toolc-reference-*.jar'
-readonly libs='lib/refparser.jar:lib/cafebabe.jar'
-
-if [[ $1 = '-s' ]]
-then
-	t='true'
-	shift
-else
-	t='false'
-fi
-readonly use_sbt="$t"
+readonly tes='tvm/bin/Debug/tvm'
 
 check() {
 	local status=$1
@@ -28,74 +16,45 @@ check() {
 }
 
 compile() {
-	sbt <<< ';package;exit' > /dev/null
+	local f="${1}"
 
-	ls target/scala-*/toolc_*.jar
+	java -jar ${ref} "$f"
+	check $?
 }
 
 reference() {
 	local f="${1}"
 
-	java -jar ${ref} "$f" 2>&1 >/dev/null
+	java "$(echo "${f}" | cut -d . -f 1)" > "${f}_ref"
 	check $?
 }
 
 generate() {
 	local f="${1}"
-	local com="${2}"
 
-	local ret1
-	if $use_sbt
-	then
-		sbt_cmd=";compile; run $f;exit"
-		sbt <<< "$sbt_cmd" | \
-			grep -vE "$sbt_cleanup|^> $sbt_cmd$" \
-				2>&1 >/dev/null
-		ret1=$?
-	else
-		scala -cp "$com" 'toolc.Main' "$f" \
-			2>&1 >/dev/null
-		ret1=$?
-	fi
-	check $ret1
+	${tes} "${f}" 2>/dev/null > "${f}_tes"
+	check $?
 }
 
 match() {
 	local com="$1"
 	local f="$2"
 
-	local l=$(grep -hE '^\s*(class|object)' "${f}" | \
+	local l=$(grep -hE '^\s*(object)' "${f}" | \
 		sed 's/^\s*//' | \
 		cut -d ' ' -f 2 | \
-		tr -d '\r{')
+		tr -d '\r{').class
 
 	echo -n "Testing: $f "
+	compile "${f}"
 
-	reference "${f}"
-	for p in ${l}
-	do
-		javap -c "${p}" > "${p}_ref"
-		check $?
-	done
-	generate "${f}" "${com}"
+	reference "${l}"
+	generate "${l}"
 
-	for p in ${l}
-	do
-		javap -c "${p}" > "${p}_tes"
-		diff -q "${p}_ref" "${p}_tes" > /dev/null
-		check $?
-		rm "${p}"{_{tes,ref},.class}
-	done
+	diff "${l}"_{tes,ref}
 
 	echo
 }
-
-if ! $use_sbt
-then
-	echo -n 'Compiling ...'
-	local_test="$(compile)"
-	echo
-fi
 
 if [ $# -ne 0 ]
 then
